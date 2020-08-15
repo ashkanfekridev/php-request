1e65ccb9818e8ab6ab3659532355aa336d4fd897
ashkanfekridev <ashkanfekridev@gmail.com>
2020-08-15 23:02:57 +0430

    add class to file

--- src/Request.php
+++ src/Request.php
@@ -1,7 +1,22 @@
 <?php
+
 namespace Ashkanfekri\dodo;
 
 class Request
 {
+  /**
+   * @return string
+   */
+  public static function url()
+  {
+    return trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
+  }
 
+  /**
+   * @return mixed
+   */
+  public static function method()
+  {
+    return $_SERVER['REQUEST_METHOD'];
+  }
 }
